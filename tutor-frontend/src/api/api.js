a79dc945165a9a58a00311d2caa5018a043f09aa
Oyelola Adeboye <78259921+Boye-dev@users.noreply.github.com>
2024-02-02 15:50:55 +0100

    feat: update-app-build

--- tutor-frontend/src/api/api.js
+++ tutor-frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export default axios.create({
   // baseURL: "http://localhost:4000",
-  baseURL: "https://tutorial-app.up.railway.app",
+  baseURL: "https://tutorial-app.onrender.com",
   withCredentials: true,
   credentials: "include",
 });
